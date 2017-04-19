d9ff98f5e05a113332c790b680f23939bacc024f
serenasangeunlee <serena.lee@kellogg.northwestern.edu>
2017-04-19 18:20:32 -0500

    update

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -27,7 +27,7 @@ class CalculationsController < ApplicationController
   end
 
   def payment(r,n,p)
-    pmt=(p*(r/12))/(1-(1+r/12)**-n)
+    pmt=p*(r/12)/(1-(1+r/12)**-(n*12))
   end
 
   def loan_payment
@@ -64,18 +64,54 @@ class CalculationsController < ApplicationController
     #   number of seconds as a result.
     # ================================================================================
 
-    @seconds = "Replace this string with your answer."
-    @minutes = "Replace this string with your answer."
-    @hours = "Replace this string with your answer."
-    @days = "Replace this string with your answer."
-    @weeks = "Replace this string with your answer."
-    @years = "Replace this string with your answer."
+    @seconds = @ending - @starting
+    @minutes = (@ending - @starting)/60
+    @hours = (@ending - @starting)/60/60
+    @days = (@ending - @starting)/60/60/24
+    @weeks = (@ending - @starting)/60/60/24/7
+    @years = (@ending - @starting)/60/60/24/7/52
 
     # ================================================================================
     # Your code goes above.
     # ================================================================================
 
     render("time_between.html.erb")
+  end
+
+  def sum(n)
+    n.inject(0) { |sum, x| sum + x }
+  end
+
+  def mean(n)
+    n.inject(0) {sum(n)} / n.size.to_f
+  end
+
+  def median(n, already_sorted=false)
+    return nil if n.empty?
+    n = n.sort unless already_sorted
+    m_pos = n.size / 2
+    return n.size % 2 == 1 ? n[m_pos] : mean(n[m_pos-1..m_pos])
+  end
+
+  def variance(n)
+    m = mean(n)
+    n.inject(0) { |variance, x| variance + ((x - m) **2) }
+  end
+
+  def standard_deviation(n)
+    m = mean(n)
+    v = variance(n)
+    return m, Math.sqrt(v/(n.size-1))
+  end
+
+  def modes(n, find_all=true)
+    histogram = n.inject(Hash.new(0)) { |h, n| h[n] += 1; h }
+    modes = nil
+    histogram.each_pair do |item, times|
+      modes << item if modes && times == modes[0] and find_all
+      modes = [times, item] if (!modes && times>1) or (modes && times>modes[0])
+    end
+    return modes ? modes[1...modes.size] : modes
   end
 
   def descriptive_statistics
@@ -86,27 +122,36 @@ class CalculationsController < ApplicationController
     # The numbers the user input are in the array @numbers.
     # ================================================================================
 
-    @sorted_numbers = "Replace this string with your answer."
+    @sorted_numbers = @numbers.sort
 
-    @count = "Replace this string with your answer."
+    @count = @numbers.count
 
-    @minimum = "Replace this string with your answer."
+    @minimum = @numbers.min
 
-    @maximum = "Replace this string with your answer."
+    @maximum = @numbers.max
 
-    @range = "Replace this string with your answer."
+    @range = @maximum - @minimum
 
-    @median = "Replace this string with your answer."
+    @list = @sorted_numbers.length
+    if @list %2 != 0
+      @median = @sorted_numbers[@list/2]
+    else
+      @median = (@sorted_numbers[@list/2]+@sorted_numbers[(@list/2)-1])/2.0
+    end
 
-    @sum = "Replace this string with your answer."
+    @sum = @numbers.sum
 
-    @mean = "Replace this string with your answer."
+    @mean = @sum / @count
 
-    @variance = "Replace this string with your answer."
+    @variance = 0
+    @numbers.each do |x|
+      @variance = @variance + ((x-@mean)**2)/@count
+    end
 
-    @standard_deviation = "Replace this string with your answer."
+    @standard_deviation = Math.sqrt(@variance)
 
-    @mode = "Replace this string with your answer."
+    @freq = @numbers.inject(Hash.new(0)) {|h,v| h[v] += 1; h}
+    @mode=@numbers.max_by { |v| @freq[v]}
 
     # ================================================================================
     # Your code goes above.
